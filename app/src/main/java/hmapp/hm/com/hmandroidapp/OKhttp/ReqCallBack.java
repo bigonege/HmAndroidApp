734f21d16636b10adad0717c0b7f4d203e548cb8
wangky1025 <373721041@qq.com>
2018-06-24 12:28:25 +0800

    保存数据

---
+++ app/src/main/java/hmapp/hm/com/hmandroidapp/OKhttp/ReqCallBack.java
@@ -0,0 +1,13 @@
+package hmapp.hm.com.hmandroidapp.OKhttp;
+
+public interface ReqCallBack<T> {
+    /**
+     * 响应成功
+     */
+    void onReqSuccess(T result);
+
+    /**
+     * 响应失败
+     */
+    void onReqFailed(String errorMsg);
+}
